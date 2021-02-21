782958e72454dfd27ae566353fc03f42aa786302
jeronimoalbano <jeronimo.jalbano@opovodigital.com>
2021-02-21 19:12:29 -0300

    chore: Adicionado API de autenticação

--- frontend/src/app/account/shared/account.service.ts
+++ frontend/src/app/account/shared/account.service.ts
@@ -1,16 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../../../environments/environment';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
-
-  login(user: any){
-    return new Promise((resolve, reject)=>{
-      window.localStorage.setItem('token','meu-token');
-      resolve(true);
-    })
+  async login(user: any){
+    const result = await this.http.post<any>(`${environment.api}/api/auth/login`, user).toPromise();
+    console.log(result)
+    if(result && result.access_token){
+      window.localStorage.setItem('token','meu-token')
+      return true;
+    }
+    return false;
   }
 }
