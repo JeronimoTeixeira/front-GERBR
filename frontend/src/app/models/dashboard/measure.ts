92efe3f7bb56a6fedaa1c3ec583bfa852bf64a7e
JeronimoTeixeira <geteixeira095@gmail.com>
2021-05-20 11:33:51 -0300

    chore: modelos padrões

---
+++ frontend/src/app/models/dashboard/measure.ts
@@ -0,0 +1,8 @@
+export class Measure{
+    id: Number
+    id_measurer: Number
+    value: Number
+    date: String
+    created_at: String
+    updated_at: String
+}
---
+++ frontend/src/app/models/dashboard/measureAddress.ts
@@ -0,0 +1,4 @@
+export class MeasureAddress{
+    id: Number
+    address: String
+}
